5276a2246905878d28ddb23c1ba47e2ce74ce3a9
maasymaas16 <maasymaas16@gmail.com>
2022-09-28 21:53:38 -0600

    Added click event listener to add marker to clicked box on gameboard

--- myscript.js
+++ myscript.js
@@ -7,12 +7,19 @@ const Gameboard = (() => {
     for (i = 0; i < 9; i++){
         const box = document.createElement('div');
         box.className = 'gameBoardBox';
-        box.setAttribute('id', 'box'+ i);
+        box.setAttribute('id', 'box');
         const board = document.querySelector('.board-grid');
         board.appendChild(box);
-    
     }
 
+    const placeMarker = function(){
+        document.addEventListener('click', function(e){
+            if(e.target && e.target.id==='box'){
+                e.target.innerHTML = 'X';
+            }
+          });
+    }();
+    
 })();
 
 const Game = (() => {
