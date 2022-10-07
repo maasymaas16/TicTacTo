68dd7f1a434579ef31e00ad26b5e64cf70040625
maasymaas16 <maasymaas16@gmail.com>
2022-10-06 21:56:33 -0600

    Added Next round button to press after round has been won/lost/tied

--- myscript.js
+++ myscript.js
@@ -15,16 +15,18 @@ const playerFactory = (name) => {
         return score;
     }
 
-    return { name, incrementScore, getScore, marker, turn, resetScore};
+    return { name, incrementScore, getScore, marker, turn, resetScore };
 };
+
+
 
 const Gameboard = (() => {
     var maxTurns = 9;
     var gameArray = new Array(9).fill(0);
-    for (i = 0; i < maxTurns; i++){
+    for (i = 0; i < maxTurns; i++) {
         const box = document.createElement('div');
         box.className = 'gameBoardBox';
-        box.setAttribute("value",i);
+        box.setAttribute("value", i);
         box.setAttribute('id', 'box');
         const board = document.querySelector('.board-grid');
         board.appendChild(box);
@@ -32,12 +34,13 @@ const Gameboard = (() => {
 
     function clearBoard() {
         let boxes = document.getElementsByClassName('gameBoardBox');
-        for (i = 0; i < boxes.length; i++){
+        for (i = 0; i < boxes.length; i++) {
             boxes[i].innerHTML = '';
         }
-        for (j = 0; j < gameArray.length; j++){
+        for (j = 0; j < gameArray.length; j++) {
             gameArray[j] = 0;
         }
+        Game.winner.style.display = 'none';
     }
 
     function resetGame() {
@@ -48,7 +51,7 @@ const Gameboard = (() => {
         updateScore();
     }
 
-    function updateScore(){
+    function updateScore() {
         const p1Score = document.querySelector('.p1Score');
         const p2Score = document.querySelector('.p2Score');
         p1Score.innerHTML = Game.playerOne.getScore();
@@ -56,8 +59,9 @@ const Gameboard = (() => {
     }
 
     return { gameArray, resetGame, updateScore, clearBoard };
-    
+
 })();
+
 
 
 const Game = (() => {
@@ -67,13 +71,15 @@ const Game = (() => {
     playerTwo.marker = 'O';
     playerOne.turn = true;
     let turnNum = 0;
+    let winner = document.getElementById('winner');
+    winner.style.display = 'none';
 
     function resetTurn() {
         turnNum = 0;
     }
 
-    function switchTurn(){
-        if (playerOne.turn){
+    function switchTurn() {
+        if (playerOne.turn) {
             playerOne.turn = !playerOne.turn;
             playerTwo.turn = !playerTwo.turn;
         } else {
@@ -83,48 +89,55 @@ const Game = (() => {
         turnNum += 1;
     }
 
+    function displayWinner() {
+        if(playerOne.turn){
+            winner.innerHTML = 'Player1 Wins!';
+            winner.style.display = 'block';
+        } else {
+            winner.innerHTML = 'Player2 Wins!';
+            winner.style.display = 'block';
+        }
+    }
+    
     function checkWinner() {
-        for (j=0; j<3; j++){
-                if (Gameboard.gameArray[j] != 0 && ((Gameboard.gameArray[j] === Gameboard.gameArray[j+3]) && (Gameboard.gameArray[j+3] === Gameboard.gameArray[j+6]))){
-                    alert('1');
-                    return true;
-                } else if (Gameboard.gameArray[(j*3)] != 0 && (Gameboard.gameArray[(j*3)] === Gameboard.gameArray[(j*3)+1]) && (Gameboard.gameArray[(j*3)+1] === Gameboard.gameArray[(j*3)+2])){
-                    alert('2');
-                    return true;
-                } else if (Gameboard.gameArray[0] != 0 && ((Gameboard.gameArray[0] === Gameboard.gameArray[4]) && (Gameboard.gameArray[4] === Gameboard.gameArray[8]))) { 
-                    alert('3');
-                    return true;
-                } else if (Gameboard.gameArray[2] != 0 && ((Gameboard.gameArray[2] === Gameboard.gameArray[4]) && (Gameboard.gameArray[4] === Gameboard.gameArray[6]))) {
-                    alert('4');
-                    return true;
-                }
+        for (j = 0; j < 3; j++) {
+            if (Gameboard.gameArray[j] != 0 && ((Gameboard.gameArray[j] === Gameboard.gameArray[j + 3]) && (Gameboard.gameArray[j + 3] === Gameboard.gameArray[j + 6]))) {
+                return true;
+            } else if (Gameboard.gameArray[(j * 3)] != 0 && (Gameboard.gameArray[(j * 3)] === Gameboard.gameArray[(j * 3) + 1]) && (Gameboard.gameArray[(j * 3) + 1] === Gameboard.gameArray[(j * 3) + 2])) {
+                return true;
+            } else if (Gameboard.gameArray[0] != 0 && ((Gameboard.gameArray[0] === Gameboard.gameArray[4]) && (Gameboard.gameArray[4] === Gameboard.gameArray[8]))) {
+                return true;
+            } else if (Gameboard.gameArray[2] != 0 && ((Gameboard.gameArray[2] === Gameboard.gameArray[4]) && (Gameboard.gameArray[4] === Gameboard.gameArray[6]))) {
+                return true;
             }
+        }
         return false;
     }
 
-    document.addEventListener('click', function(e){
-        if(e.target && e.target.id == 'box' && e.target.innerHTML == ''){
+    document.addEventListener('click', function (e) {
+        if (e.target && e.target.id == 'box' && e.target.innerHTML == '') {
             Gameboard.gameArray[e.target.getAttribute('value')] = (playerOne.turn) ? 'X' : 'O';
             e.target.innerHTML = ((playerOne.turn) ? 'X' : 'O');
-            if (checkWinner()){
-                playerOne.turn ? alert('Player1 wins!') : alert('Player2 wins!');
+            if (checkWinner()) {
                 playerOne.turn ? playerOne.incrementScore() : playerTwo.incrementScore();
+                displayWinner();
                 Gameboard.updateScore();
-                Gameboard.clearBoard();
-                turnNum = 0;  
-            } else if (turnNum == 8){
-                Gameboard.clearBoard();
                 turnNum = 0;
-                alert('Tie Game!')
+                playerOne.turn = true;
+                playerTwo.turn = false;
+            } else if (turnNum == 8) {
+                winner.innerHTML = 'Tie game!';
+                winner.style.display = 'block';
+                turnNum = 0;
             } else {
                 switchTurn();
             }
-        } else if (e.target.id==='box' && e.target.innerHTML != '') {
+        } else if (e.target.id === 'box' && e.target.innerHTML != '') {
             alert('Please pick an empty square');
         }
     });
 
-    return { resetTurn, playerOne, playerTwo };
+    return { resetTurn, playerOne, playerTwo, winner };
 
 })();
 
